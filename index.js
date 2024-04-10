179326c5d2c1f4fe80be5405e6929a8d68297e94
EmdadulHoque1 <lovelyimrad@gmail.com>
2024-04-10 20:55:43 +0600

    Solutions-1

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Complete the square sum function so that it squares each number passed into it and then sums the results together.
- 
+// Solutions-1
+const num = [1,2,2]
+const sum = num.reduce(function(acc, cv){
+    // return acc + (cv * cv)
+    return acc + Math.pow(2,cv)
+})
+console.log(sum);
