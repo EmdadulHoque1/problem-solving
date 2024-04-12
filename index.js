cdc179c06e5d35df01fdcad481d96cd2c15d63cc
EmdadulHoque1 <lovelyimrad@gmail.com>
2024-04-12 16:26:33 +0600

    Solutions-1 edited

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Complete the square sum function so that it squares each number passed into it and then sums the results together.
-// Solutions-1
+// -Solutions-1
  
 const num = [1,2,2]
 const sum = num.reduce(function(acc,cv){
